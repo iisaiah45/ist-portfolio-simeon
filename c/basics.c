2618c7b75f5cdfc1b6b86a28687e05109c50322e
isaiah <2027simeon.isaiah@fcboe.org>
2024-01-22 14:50:24 -0500

    finished basics code

---
+++ c/basics.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+        printf (" this is my c basics file");
+
+	int num;
+	printf("enter a number: ");
+        scanf("%i", &num);
+	printf("Your number was %i\n", num);
+	return 0;
+}
